397384843a7d18bdacfb81827d54e5be7fd72d33
Windawz <46968683+Windawz@users.noreply.github.com>
2023-02-08 13:16:01 +0300

    Add form that opens the diamond form

---
+++ WinFormsTasks/WinFormsTasks.Task7/DiamondFormHostForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WinFormsTasks.Common;
+
+namespace WinFormsTasks.Task7;
+[SelectableForm("Diamond Form Opener")]
+public partial class DiamondFormHostForm : Form {
+    public DiamondFormHostForm() {
+        InitializeComponent();
+
+        var openerButton = new Button() {
+            AutoSize = true,
+            Dock = DockStyle.Top,
+            Text = "Open Diamond Form",
+        };
+
+        void OpenDiamondForm(object? sender, EventArgs e) {
+            var diamondForm = new DiamondForm();
+            AddOwnedForm(diamondForm);
+            diamondForm.FormClosed += delegate {
+                openerButton.Click += OpenDiamondForm;
+            };
+            openerButton.Click -= OpenDiamondForm;
+            diamondForm.Show();
+        };
+
+        openerButton.Click += OpenDiamondForm;
+        Controls.Add(openerButton);
+    }
+}
