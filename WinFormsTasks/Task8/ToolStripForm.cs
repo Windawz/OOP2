b2c8d3265e045a9f10ae15d8c792584f82a0e1d7
Windawz <46968683+Windawz@users.noreply.github.com>
2023-02-15 10:36:48 +0300

    Add ToolStripForm from excercise 3

---
+++ WinFormsTasks/Task8/ToolStripForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WinFormsTasks.Common;
+using WinFormsTasks.Task8.Properties;
+
+namespace WinFormsTasks.Task8;
+[SelectableForm("Tool Strip Form (Excercise 3)", 3)]
+public partial class ToolStripForm : MdiParentForm {
+    public ToolStripForm() {
+        InitializeComponent();
+        
+        var panel = new Panel() { Dock = DockStyle.Top, Height = 30, };
+        var splitter = new Splitter() { Dock = DockStyle.Top, };
+
+        Controls.Add(splitter);
+        Controls.Add(panel);
+
+        var toolStripContainer = new ToolStripContainer() { Dock = DockStyle.Top, };
+        panel.Controls.Add(toolStripContainer);
+
+        var toolStrip = new ToolStrip() { };
+        toolStripContainer.TopToolStripPanel.Controls.Add(toolStrip);
+
+        var newButton = new ToolStripButton() {
+            AutoSize = true,
+            Image = Resources.icon01,
+            Text = "New",
+        };
+        newButton.Click += delegate {
+            AddSubmenu.PerformClick();
+        };
+        toolStrip.Items.Add(newButton);
+
+        toolStrip.Items.Add(new ToolStripSeparator());
+
+        var windowsCascadeButton = new ToolStripButton() {
+            AutoSize = true,
+            Image = Resources.icon02,
+            Text = "Cascade",
+        };
+        windowsCascadeButton.Click += delegate {
+            LayoutMdi(MdiLayout.Cascade);
+        };
+        toolStrip.Items.Add(windowsCascadeButton);
+
+        toolStrip.Items.Add(new ToolStripSeparator());
+
+        var windowsTileButton = new ToolStripButton() {
+            AutoSize = true,
+            Image = Resources.icon03,
+            Text = "Tile Horizontally",
+        };
+        windowsTileButton.Click += delegate {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        };
+        toolStrip.Items.Add(windowsTileButton);
+
+        toolStrip.Items.Add(new ToolStripSeparator());
+
+        var closeAllButton = new ToolStripButton() {
+            AutoSize = true,
+            Text = "Close All",
+        };
+        closeAllButton.Click += delegate {
+            foreach (var child in MdiChildren) {
+                child.Close();
+            }
+        };
+        toolStrip.Items.Add(closeAllButton);
+    }
+}
