32dc7ce76dd4bcaee1168cc5cfa240fd2371b5b1
Windawz <46968683+Windawz@users.noreply.github.com>
2023-03-01 10:12:47 +0300

    Add timer custom control and a form for it

---
+++ WinFormsTasks/WinFormsTasks.Task9/TimerCustomControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTasks.Task9;
+public partial class TimerCustomControl : Control {
+    public TimerCustomControl() {
+        InitializeComponent();
+
+        Size = new(60, 20);
+
+        _timer = new Timer() {
+            Interval = 1000,
+        };
+        _timer.Tick += delegate {
+            Refresh();
+        };
+        components!.Add(_timer);
+    }
+
+    private readonly Timer _timer;
+
+    public bool TimerEnabled {
+        get => _timer.Enabled;
+        set => _timer.Enabled = value;
+    }
+
+    protected override void OnPaint(PaintEventArgs pe) {
+        base.OnPaint(pe);
+        var graphics = pe.Graphics;
+        graphics.FillRectangle(
+            Brushes.Blue,
+            0,
+            0,
+            Width,
+            Height);
+
+        string timeString = DateTime.Now.ToLongTimeString();
+        graphics.DrawString(
+            timeString,
+            Font,
+            new SolidBrush(ForeColor),
+            0,
+            0);
+    }
+}
---
+++ WinFormsTasks/WinFormsTasks.Task9/TimerCustomControlForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WinFormsTasks.Common;
+
+namespace WinFormsTasks.Task9;
+[SelectableForm]
+public partial class TimerCustomControlForm : Form {
+    public TimerCustomControlForm() {
+        InitializeComponent();
+
+        var timerCustomControl = new TimerCustomControl() {
+            Location = new(Size.Width / 2, Size.Height / 2),
+            TimerEnabled = true,
+        };
+        Controls.Add(timerCustomControl);
+    }
+}
