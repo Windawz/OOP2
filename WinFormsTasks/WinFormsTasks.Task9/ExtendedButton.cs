5cf45ac87c8bef831e0c04040b32efee5c40adfc
Windawz <46968683+Windawz@users.noreply.github.com>
2023-03-01 10:22:18 +0300

    Add extended button control and a form for it

---
+++ WinFormsTasks/WinFormsTasks.Task9/ExtendedButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTasks.Task9;
+internal class ExtendedButton : Button {
+    public int ClickCount { get; private set; } = 0;
+
+    protected override void OnClick(EventArgs e) {
+        ClickCount += 1;
+        base.OnClick(e);
+    }
+
+    protected override void OnPaint(PaintEventArgs pevent) {
+        base.OnPaint(pevent);
+
+        Graphics graphics = pevent.Graphics;
+        string clickCountString = ClickCount.ToString();
+        SizeF size = graphics.MeasureString(clickCountString, Font, Width);
+        graphics.DrawString(
+            clickCountString,
+            Font,
+            SystemBrushes.ControlText,
+            Width - size.Width - 3,
+            Height - size.Height - 3);
+    }
+}
---
+++ WinFormsTasks/WinFormsTasks.Task9/ExtendedButtonForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WinFormsTasks.Common;
+
+namespace WinFormsTasks.Task9;
+[SelectableForm]
+public partial class ExtendedButtonForm : Form {
+    public ExtendedButtonForm() {
+        InitializeComponent();
+
+        var extendedButton = new ExtendedButton() {
+            Text = "Click Me!",
+            Size = new(180, 75),
+        };
+        Controls.Add(extendedButton);
+    }
+}
